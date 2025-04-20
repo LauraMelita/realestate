624e6893ebde9e4cf37d36bb7ec717182f287319
lauraMelita <melita_laura@hotmail.com>
2025-04-20 09:16:25 +0200

    (refactor): centralize zipId configs and clean up model validation helpers

--- src/scrapers/era/constants.js
+++ src/scrapers/era/constants.js
@@ -7,10 +7,16 @@ const ERA_CONFIG = {
     apartment: '44',
   },
   zipIds: {
-    1083: 2879, // Ganshoren
+    1030: 2875, // Schaerbeek
+    1060: 2884, // Saint-Gilles
+    1070: 2876, // Anderlecht
+    1080: 2885, // Molenbeek-Saint-Jean
     1081: 2881, // Koekelberg
-    1090: 2880, // Jette
     1082: 2883, // Berchem-Sainte-Agathe
+    1083: 2879, // Ganshoren
+    1090: 2880, // Jette
+    1160: 2882, // Auderghem
+    1210: 2886, // Saint-Josse-ten-Noode
   },
   selectors: {
     link: '.rs-canonical-link-formatter a',
