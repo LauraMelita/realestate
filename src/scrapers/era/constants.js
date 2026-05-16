405acd9558ff58dd15ad8fd63cb53f3dc9903c70
Laura Melita <71453198+LauraMelita@users.noreply.github.com>
2026-05-16 13:38:19 +0200

    (chore): restore zipId mapping

--- src/scrapers/era/constants.js
+++ src/scrapers/era/constants.js
@@ -2,11 +2,17 @@ const ERA_CONFIG = {
   apiUrl: 'https://www.era.be/fr/jsonapi/index/property_index',
   baseUrl: 'https://www.era.be',
   zipIds: {
+    1030: 2875, // Schaerbeek
+    1040: 2878, // Etterbeek
+    1060: 2884, // Saint-Gilles
+    1070: 2876, // Anderlecht
     1080: 2885, // Molenbeek-Saint-Jean
     1081: 2881, // Koekelberg
     1082: 2883, // Berchem-Sainte-Agathe
     1083: 2879, // Ganshoren
     1090: 2880, // Jette
+    1160: 2882, // Auderghem
+    1210: 2886, // Saint-Josse-ten-Noode
   },
   selectors: {
     link: '.rs-canonical-link-formatter a',
